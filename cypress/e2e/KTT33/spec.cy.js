1a0df3636012fcd7b66281f9e3cb5fa871e71600
carlosmora97 <carlosmora1997@gmail.com>
2022-11-27 21:15:21 -0600

    Create spec.cy.js

---
+++ cypress/e2e/KTT33/spec.cy.js
@@ -0,0 +1,11 @@
+describe('empty spec', () => {
+  it('Probar pantalla principal "Buscando viaje"', () => {
+    cy.visit('https://kane-taxi.web.app');
+    cy.get('input[name="email"]').type("testtaxi@mailinator.com");
+    cy.get('input[name="password"]').type("test123.");
+    cy.contains("Iniciar sesión").click();
+    cy.wait(1000);
+    cy.contains("Buscando viaje")
+  });
+
+})
