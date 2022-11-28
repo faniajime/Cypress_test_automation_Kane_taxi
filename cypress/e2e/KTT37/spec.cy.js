4b33df3a96c0438255d6510b837d9b68781b31b0
carlosespinoza <carlosespinoza400@outlook.com>
2022-11-27 19:31:44 -0600

    Prueba para detectar botón de pánico

---
+++ cypress/e2e/KTT37/spec.cy.js
@@ -0,0 +1,29 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://kane-taxi.web.app/')
+
+    cy.wait(2000)
+
+    //Test input email
+    cy.get('[formcontrolname="email"] > .native-input').type('testtaxi@mailinator.com')
+    
+    cy.wait(2000)
+
+
+    //Test password
+    cy.get('.ng-pristine > .native-input').type('test123.')
+
+    cy.wait(2000)
+
+
+    //Login btn
+    cy.get('.center > .ion-color').click()
+
+    cy.wait(4000)
+
+    //Test Panic Btn
+    cy.get('.fab-horizontal-start > .ion-color').should('be.visible')
+
+
+  })
+})
