60adaa144dc771978d278947103e7f26142c7f7a
carlosmora97 <carlosmora1997@gmail.com>
2022-11-27 21:31:41 -0600

    KTT 33. Probar pantalla principal "Buscando viaje"

--- cypress/e2e/KTT33/spec.cy.js
+++ cypress/e2e/KTT33/spec.cy.js
@@ -1,5 +1,5 @@
 describe('empty spec', () => {
-  it('Probar pantalla principal "Buscando viaje"', () => {
+  it('Probar pantalla principal "Buscando viaje"(KTT33)', () => {
     cy.visit('https://kane-taxi.web.app');
     cy.get('input[name="email"]').type("testtaxi@mailinator.com");
     cy.get('input[name="password"]').type("test123.");
