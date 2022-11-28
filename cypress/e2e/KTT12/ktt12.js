a487d18f589f6396e223cd0c30b0d210a80645ec
carlosespinoza <carlosespinoza400@outlook.com>
2022-11-27 21:31:03 -0600

    Corrections

---
+++ cypress/e2e/KTT12/ktt12.js
@@ -0,0 +1,32 @@
+describe('user needs the history option', () => {
+  it('login', () => {
+    cy.visit('https://kane-taxi.web.app/')
+
+    cy.wait(2000)
+
+    //Test input email
+    cy.get('[formcontrolname="email"] > .native-input').type('testtaxi@mailinator.com')
+    
+    cy.wait(2000)
+
+
+    //Test password
+    cy.get('.ng-pristine > .native-input').type('test123.')
+
+    cy.wait(2000)
+
+
+    //Login btn
+    cy.get('.center > .ion-color').click()
+
+    cy.wait(2000)
+
+    //Test last trip
+    cy.get('.title-default > :nth-child(2)').should('be.visible')
+
+  })
+
+  it('logout', () =>{
+    cy.get(".log-out").click();
+  })
+})
