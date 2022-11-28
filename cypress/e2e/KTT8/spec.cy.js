0d190502829683b1f8ebe89f7622b612cf476107
carlosmora97 <carlosmora1997@gmail.com>
2022-11-27 22:23:39 -0600

    Change name to tag on Jira

--- cypress/e2e/KTT8/spec.cy.js
+++ cypress/e2e/KTT8/spec.cy.js
@@ -1,5 +1,5 @@
 describe('empty spec', () => {
-  it('Probar pantalla principal "Buscando viaje"(KTT33)', () => {
+  it('Probar pantalla principal "Buscando viaje"(KT8)', () => {
     cy.visit('https://kane-taxi.web.app');
     cy.get('input[name="email"]').type("testtaxi@mailinator.com");
     cy.get('input[name="password"]').type("test123.");
